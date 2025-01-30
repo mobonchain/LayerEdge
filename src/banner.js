e9da086c3bc90843a13ad25200ddf435b43d1a08
MobGM <81332044+mobonchain@users.noreply.github.com>
2025-01-30 11:46:42 +0700

    Create banner.js

---
+++ src/banner.js
@@ -0,0 +1,10 @@
+import chalk from 'chalk';
+
+const banner = `
+${chalk.white('════════════════════════════════════════════════════════════════')}
+                        ${chalk.bold.cyan('LayerEdge LightNode')}
+           ${chalk.yellow('-- Tool shared by GitHub user @airdropinsiders --')}
+${chalk.white('════════════════════════════════════════════════════════════════')}
+`;
+
+export default banner;
